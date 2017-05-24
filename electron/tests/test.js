9bb7799bd65539a0fab763baac6ca66496191bd5
Kewin Brandsma <kewin@frontend-freelance.com>
2017-05-24 16:30:48 +0200

    fixed tests

--- electron/tests/test.js
+++ electron/tests/test.js
@@ -3,111 +3,109 @@ const path = require('path');
 const chai = require('chai');
 const fs = require('fs');
 const chaiAsPromised = require('chai-as-promised');
+const rimraf = require('rimraf');
 
 var electronPath = '',
-    configPath = '';
+	configPath = '',
+	cachePath = '';
 
 if (/^win/.test(process.platform)) {
-    electronPath = path.join(__dirname, '..', '..', 'dist', 'win-unpacked', 'TradeJS.exe');
+	electronPath = path.join(__dirname, '..', '..', 'dist', 'win-unpacked', 'TradeJS.exe');
+	configPath = path.join(__dirname, '..', '..', 'dist', 'win-unpacked', 'resources', 'app', '_config');
+	cachePath = path.join(__dirname, '..', '..', 'dist', 'win-unpacked', 'resources', 'app', '_cache');
 } else if (/^darwin/.test(process.platform)) {
-    electronPath = path.join(__dirname, '..', '..', 'dist', 'mac', 'TradeJS.app', 'Contents', 'MacOS', 'TradeJS');
+	electronPath = path.join(__dirname, '..', '..', 'dist', 'mac', 'TradeJS.app', 'Contents', 'MacOS', 'TradeJS');
+	configPath = path.join(__dirname, '..', '..', 'dist', 'mac', 'TradeJS.app', 'Contents', 'MacOS', 'resources', 'app', '_config');
+	cachePath = path.join(__dirname, '..', '..', 'dist', 'mac', 'TradeJS.app', 'Contents', 'MacOS', 'resources', 'app', '_cache');
 } else if (/^linux/.test(process.platform)) {
-    electronPath = path.join(__dirname, '..', '..', 'dist', 'linux-unpacked', 'tradejs');
+	electronPath = path.join(__dirname, '..', '..', 'dist', 'linux-unpacked', 'tradejs');
+	configPath = path.join(__dirname, '..', '..', 'dist', 'linux-unpacked', 'resources', 'app', '_config');
+	cachePath = path.join(__dirname, '..', '..', 'dist', 'linux-unpacked', 'resources', 'app', '_cache');
 }
+
+var loginDetails = {
+	accountId: 1218398,
+    token: '067331173f67faf3cef7e69263a3015a-fefb596cddfe98d2f24e9ca843c3c443'
+};
 
 var appPath = path.join(__dirname, '..', '..');
 
 var app = new Application({
-    path: electronPath,
-    args: [appPath, 'NODE_ENV=production']
+	path: electronPath,
+	args: [appPath, 'NODE_ENV=production']
 });
 
 global.before(function () {
-    chai.should();
-    chai.use(chaiAsPromised);
+	chai.should();
+	chai.use(chaiAsPromised);
+});
+
+global.beforeEach(function () {
+	return app.start();
+});
+
+global.afterEach(function () {
+	return app.stop().then(() => {
+		rimraf.sync(configPath);
+		rimraf.sync(cachePath);
+	});
 });
 
 describe('Window', function () {
-    beforeEach(function () {
-        return app.start();
-    });
 
-    afterEach(function () {
-        fs.unlinkSync(path.join(electronPath, '..', 'resources', 'app', '_config', 'tradejs.config.json'));
-        return app.stop();
-    });
+	it('opens a window', function () {
+		return app.client
+			.waitUntilWindowLoaded()
+			.getWindowCount().should.eventually.equal(1);
+	});
 
-    it('opens a window', function () {
-        return app.client.waitUntilWindowLoaded()
-            .getWindowCount().should.eventually.equal(1);
-    });
-
-    it('tests the title', function () {
-        return app.client.waitUntilWindowLoaded()
-            .getTitle().should.eventually.equal('TradeJS - Practise');
-    });
+	it('tests the title', function () {
+		return app.client
+			.waitUntilWindowLoaded()
+			.getTitle().should.eventually.equal('TradeJS - Practise');
+	});
 });
 
 describe('Server Connection', function () {
-    beforeEach(function () {
-        return app.start()
-    });
 
-    afterEach(function () {
-        return app.stop();
-    });
-
-    it('connects to server', function () {
-        return app.client.waitUntilWindowLoaded()
-            .waitForExist('#debugContainer .circle.ok', 5000);
-    });
+	it('connects to server', function () {
+		return app.client
+			.waitUntilWindowLoaded()
+			.waitForVisible('#loginModal')
+			.setValue('#loginAccountId', loginDetails.accountId)
+			.setValue('#loginToken', loginDetails.token)
+			.click('#loginModal .modal-footer .btn-primary')
+			.waitForVisible('#loginModal')
+			.waitForExist('#debugContainer .circle.ok', 5000);
+	});
 });
 
 describe('Charts', function () {
 
-    beforeEach(function () {
-        return app.start();
-    });
-
-    afterEach(function () {
-        return app.stop();
-    });
-
-    it('can open a chart', function () {
-        return app.client.waitUntilWindowLoaded()
-            .pause(10000)
-            .waitForExist('.three-column:first-child tr:first-child', 5000)
-            .click('.three-column:first-child tr:first-child td:first-child')
-            .waitForExist('.chart-overview-container chart-box', 5000);
-    });
-});
-
-describe('Backtest', function () {
-
-    beforeEach(function () {
-        return app.start();
-    });
-
-    afterEach(function () {
-        return app.stop();
-    });
-
-    it('can resize the debugger', function () {
-        return app.client.waitUntilWindowLoaded();
-    });
+	it('can open a chart', function () {
+		return app.client
+			.waitUntilWindowLoaded()
+			.waitForVisible('#loginModal')
+			.setValue('#loginAccountId', loginDetails.accountId)
+			.setValue('#loginToken', loginDetails.token)
+			.click('#loginModal .modal-footer .btn-primary')
+			.waitForExist('#debugContainer .circle.ok', 5000)
+			.pause(500)
+			.waitForExist('.three-column:first-child tr:first-child', 5000)
+			.click('.three-column:first-child tr:first-child td:first-child')
+			.waitForExist('.chart-overview-container chart-box', 5000);
+	});
 });
 
 describe('Editor', function () {
 
-    beforeEach(function () {
-        return app.start();
-    });
-
-    afterEach(function () {
-        return app.stop();
-    });
-
-    it('can open a chart', function () {
-        return app.client.waitUntilWindowLoaded()
-    });
+	it('can open editor window', function () {
+		return app.client
+			.waitUntilWindowLoaded()
+			.waitForVisible('.header-buttons .button-editor')
+			.click('.header-buttons .button-editor')
+			.pause(500)
+			.windowByIndex(1)
+			.waitForVisible('.editor.ace_editor', 5000);
+	});
 });
