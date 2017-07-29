30e68afc2f62d455c8b7e81093a0e98aa7a96882
Kewin Brandsma <kewin@frontend-freelance.com>
2017-07-29 21:18:45 +0200

    wip

--- client/config/webpack.prod.js
+++ client/config/webpack.prod.js
@@ -1,31 +1,27 @@
 const path = require('path');
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        publicPath: '/assets/',
-        filename: '[name].[hash].js',
-        chunkFilename: '[id].[hash].chunk.js',
-        library: 'hello-webassembly',
-        libraryTarget: 'umd',
-    },
-    externals: [
-        {
-            "window": "window"
-        }
-    ],
-
     plugins: [
         // new webpack.NoErrorsPlugin(),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-        new ExtractTextPlugin('[name].[hash].css'),
+        new UglifyJSPlugin({
+            parallel: true,
+            uglifyOptions: {
+                ie8: false,
+                ecma: 8,
+                output: {
+                    comments: false,
+                    beautify: false,
+                },
+                warnings: false
+            }
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 'ENV': JSON.stringify(ENV),
