45a71fe409408a311198570a9c377a4dd5e68565
Kewin Brandsma <kewin@frontend-freelance.com>
2017-09-26 17:46:33 +0200

    cleanup attempt

--- server-cache/src/modules/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redis = require("redis");
-const config = require('../../../tradejs.config');
-exports.client = redis.createClient(config.redis.port, config.redis.host);
-//# sourceMappingURL=redis.js.map
