6a3c63291fa3ae3e889d4babbbb2a5bbad374fe0
Kewin Brandsma <kewin@frontend-freelance.com>
2017-12-22 15:37:32 +0100

    wip

---
+++ client/res/icon/profile/profile.component.ts
@@ -0,0 +1,60 @@
+import { ChangeDetectionStrategy, Component, OnInit, Output, ViewEncapsulation, ChangeDetectorRef, HostListener } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { AlertService } from '../../services/alert.service';
+import { UserModel } from '../../models/user.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subject } from "rxjs/Subject";
+
+@Component({
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss'],
+	// encapsulation: ViewEncapsulation.Native,
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class ProfileComponent implements OnInit {
+
+	public user: UserModel = null;
+	public isSelf: boolean = true;
+
+	private _routeSub: any;
+
+	@Output() test;
+
+	constructor(
+		public userService: UserService,
+		private _changeRef: ChangeDetectorRef,
+		private _route: ActivatedRoute) {
+
+	}
+
+	ngOnInit() {
+		this.user = new UserModel({ _id: this._route.snapshot.params['id'] });
+
+		this._loadUser(this.user.options._id);
+
+		this._routeSub = this._route.params.subscribe(params => {
+			// only load if userId is different then current userId
+			if (!this.user || !this.user.options || this.user.options._id !== params['id']) {
+				this._loadUser(params['id']);
+			}
+		});
+	}
+
+	public async toggleFollow(userModel: UserModel, state: boolean) {
+		const result = await this.userService.toggleFollow(userModel, state);
+		this._changeRef.detectChanges();
+	}
+
+	private async _loadUser(userId: string) {
+		this.user = await this.userService.findById(userId, { followers: 5, copiers: 5 });
+		this.isSelf = userId === this.userService.model.options._id;
+		this._changeRef.detectChanges();
+	}
+
+	ngOnDestroy() {
+		this._routeSub.unsubscribe();
+	}
+}
---
+++ client/src/app/components/alarm-menu/alarm-menu.component.ts
@@ -0,0 +1,83 @@
+import { ChangeDetectionStrategy, Component, OnInit, Input, Output, ChangeDetectorRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { AlertService } from '../../services/alert.service';
+import { UserModel } from '../../models/user.model';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { SymbolModel } from '../../models/symbol.model';
+import { EventService } from '../../services/event.service';
+import { NgForm } from '@angular/forms';
+import { ALARM_TRIGGER_DIRECTION_DOWN, ALARM_TRIGGER_DIRECTION_UP, CUSTOM_EVENT_TYPE_ALARM } from '../../../../../shared/constants/constants';
+
+@Component({
+	selector: 'app-alarm-menu',
+	templateUrl: './alarm-menu.component.html',
+	styleUrls: ['./alarm-menu.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class AlarmMenuComponent implements OnInit {
+
+	@Input() symbol: SymbolModel;
+
+	public activeMenu = null;
+	public activeAlarmMenu = 'new';
+	public formModel: any = {
+		alarmType: "1",
+		alarm: {
+
+		}
+	};
+
+	constructor(
+		private _eventService: EventService,
+		private _changeDetectorRef: ChangeDetectorRef,
+		private _changeRef: ChangeDetectorRef,
+		private _route: ActivatedRoute) {}
+
+	ngOnInit() {
+		
+	}
+
+	toggleAlarmMenuVisibility() {
+		if (this.activeMenu === 'alarm') {
+			this.activeMenu = this.activeAlarmMenu = null;
+		} else {
+			this.activeMenu = 'alarm';
+			this.activeAlarmMenu = 'new'
+		}
+		this._changeDetectorRef.detectChanges();
+	}
+
+	toggleAlarmMenuTab(tab: string) {
+		this.activeAlarmMenu = tab;
+		this._changeDetectorRef.detectChanges();
+	}
+
+	public onCreateFormSubmit(form: NgForm) {
+		if (!this.symbol)
+			return;
+
+		this.formModel.symbol = this.symbol.options.name;
+		this.formModel.type = CUSTOM_EVENT_TYPE_ALARM;
+		this.formModel.alarm.dir = this.formModel.amount < this.symbol.options.bid ? ALARM_TRIGGER_DIRECTION_DOWN : ALARM_TRIGGER_DIRECTION_UP;
+		this._eventService.create(this.formModel);
+	}
+
+	public onClickSideMenuNumberInput(dir: number, inputEl: HTMLElement) {
+		let newValue = this.formModel.amount || this.symbol.options.bid;
+		let inc = this.symbol.options.bid / 700
+
+		if (dir > 0)
+			newValue += inc;
+		else
+			newValue -= inc;
+
+		// this.formModel.amount = parseFloat(this._priceToFixed(newValue));
+		// this._changeDetectorRef.detectChanges();
+	}
+
+	ngOnDestroy() {
+	
+	}
+}
