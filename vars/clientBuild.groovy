0d21ef994db538ec73a6bc4343b36334a7ebb100
Kewin Brandsma <kewin@frontend-freelance.com>
2018-05-17 19:29:31 +0200

    wip

--- vars/clientBuild.groovy
+++ vars/clientBuild.groovy
@@ -1,18 +1,21 @@
-node {
-    agent any
-    stage('clean workspace') {
-        cleanWs()
-    }
-    stage('get source') {
-        git branch: 'development', url: 'https://github.com/DutchKevv/TradeJS'
-    }
-    stage('build docker container') {
-        sh 'npm run build-prod-client'
-    }
-    stage('build client through docker') {
-        sh 'npm run prod-client'
-    }
-    stage('remove docker container') {
-        sh 'docker rm client'
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stage('clean workspace') {
+            cleanWs()
+        }
+        stage('get source') {
+            git branch: 'development', url: 'https://github.com/DutchKevv/TradeJS'
+        }
+        stage('build docker container') {
+            sh 'npm run build-prod-client'
+        }
+        stage('build client through docker') {
+            sh 'npm run prod-client'
+        }
+        stage('remove docker container') {
+            sh 'docker rm client'
+        }
     }
 }
