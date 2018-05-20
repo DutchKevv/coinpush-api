79ec1daa4d865f4d8b6271e22b4f6999fab3e037
Kewin Brandsma <kewin@frontend-freelance.com>
2018-05-20 20:10:45 +0200

    client fix

--- client/src/address_overwrite.ts
+++ client/src/address_overwrite.ts
@@ -8,11 +8,11 @@ const options = {
 }
 
 export default {
-    // "app": {
-    //     "secure": false,
-    //     "host": "http",
-    //     "ip": "192.168.178.11",
-    //     "port": 4000,
-    //     "ws": "ws"
-    // }
+    "app": {
+        // "secure": false,
+        // "host": "http",
+        // "ip": "192.168.178.11",
+        // "port": 4000,
+        // "ws": "ws"
+    }
 };
