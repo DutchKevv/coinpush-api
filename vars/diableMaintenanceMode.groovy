996d98c0db15d2a084835a554ec457a9d1fe108b
Kewin Brandsma <kewin@frontend-freelance.com>
2018-05-23 00:54:21 +0200

    client

--- vars/diableMaintenanceMode.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map pipelineParams) {
-    node {
-        stage('disable maintenance mode on nginx') {
-            sh 'sshpass -p Bettie123! ssh -o StrictHostKeyChecking=no kewin@coinpush.app rm -f /home/kewin/Projects/TradeJS/server-nginx/pages/maintenance_on.html'
-        }
-    }
-}
