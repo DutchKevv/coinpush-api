cf3d59cc5ff412e725f4702df57955844a312d69
Kewin Brandsma <kewin@frontend-freelance.com>
2019-02-15 20:45:39 +0100

    pull

--- client/src/polyfills.ts
+++
@@ -1,7 +0,0 @@
-(window as any).global = window;
-import 'zone.js/dist/zone';
-import 'core-js/es7/reflect';
-
-// import * as FastClick from './etc/js/fastclick.js';
-
-// FastClick.attach(document.body);
