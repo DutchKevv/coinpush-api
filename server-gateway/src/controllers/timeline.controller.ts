b84ab55f8c8f0452f8d6fdf82e990339dee5ff66
Kewin Bandsma <kewin@frontend-freelance.com>
2019-03-10 18:39:27 +0100

    wip

--- server-gateway/src/controllers/timeline.controller.ts
+++ server-gateway/src/controllers/timeline.controller.ts
@@ -4,12 +4,32 @@ import { config } from 'coinpush/src/util/util-config';
 export const timelineController = {
 
 	async get(reqUser, params?): Promise<any> {
-		return request({
-			uri: config.server.comment.apiUrl + '/timeline',
-			headers: { _id: reqUser.id },
-			qs: params,
-			json: true,
-			fullResponse: false
-		});
+		console.log('asdfasdf');
+
+		let comments: [];
+
+		try {
+			comments = await request({
+				uri: 'http://elk:9200/comments/_search',
+				headers: { _id: reqUser.id },
+				json: true,
+				fullResponse: false
+			});
+
+			comments = comments['hits']['hits'].map(comment => comment._source);
+		} catch (error) {
+			console.error(error);
+
+			comments = await request({
+				uri: config.server.comment.apiUrl + '/timeline',
+				headers: { _id: reqUser.id },
+				qs: params,
+				json: true,
+				fullResponse: false
+			});
+		}
+
+
+		return comments;
 	}
 };
